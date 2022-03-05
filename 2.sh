5e00b44e6d44f3512cee8518c251084542fcab40
ssistla <ssistla@yahoo.com>
2022-03-05 09:42:54 -0600

    qa

--- 2.sh
+++ 2.sh
@@ -8,4 +8,7 @@ systemctl start confluent-schema-registry
 systemctl start confluent-kafka-rest
 systemctl start confluent-ksql
 sleep 1000
+# testing Branches
+# Add line for QA
+#Add line for feature/develop
 systemctl start confluent-kafka-connect
